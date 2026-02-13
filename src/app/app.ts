30f6038605653435a0a6ae337ca63c5b7ed57390
rajkumar <rema1191@gmail.com>
2026-02-13 21:01:01 +0530

    Add About and Services components with basic structure and styling
    - Implement About component with template and spec file
    - Implement Services component with template and spec file
    - Update app to include new components and display date
    - Add CSS for date styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { About } from '../components/about/about';
+import { Services } from '../components/services/services';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, About, Services],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('angular-app-new');
+
+  date = new Date();
+
+  imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHfD9JWXBHo5Pn2VrDXQwfBl_aWgd7q-0iTA&s";
+
+  handleClickEvent(){
+    alert("Clicked");
+  }
 }
